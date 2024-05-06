a0ce986ad4a353cbc9feab7837115899dffa6a5b
Filipe <filiperdacas@gmail.com>
2024-05-05 21:29:40 -0300

    add UsuarioRepository

--- src/main/java/com/agenda_service_back/usuario/UsuarioRepository.java
+++ src/main/java/com/agenda_service_back/usuario/UsuarioRepository.java
@@ -1,4 +1,18 @@
 package com.agenda_service_back.usuario;
 
-public class UsuarioRepository {
+import com.agenda_service_back.endereco.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByUsuarioCpf(String cpf);
+
+    Usuario findByUsuarioEmail(String email);
+
+    List<Usuario> findByUsuarioEnderecoId(Endereco endereco);
 }
+
